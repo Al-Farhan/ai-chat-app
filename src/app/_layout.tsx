1ed8befe707c25070d41f663b4dd6f427827ca4c
Farhan Shaikh <programmingwithfarhan@gmail.com>
2025-09-03 19:28:33 +0530

    Drawer

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,43 @@
-import { Slot, Stack } from "expo-router";
+import { Stack } from "expo-router";
 import "../../global.css";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { Drawer } from "expo-router/drawer";
+import { StyleSheet } from "react-native";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { DarkTheme, ThemeProvider } from "@react-navigation/native";
 export default function RootLayout() {
-  return <Stack />;
+  const myTheme = {
+    ...DarkTheme,
+    colors: {
+      ...DarkTheme.colors,
+      primary: "white",
+    },
+  };
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <ThemeProvider value={myTheme}>
+        <Drawer
+          screenOptions={{
+            headerTitle: "",
+            headerStyle: { backgroundColor: "black" },
+            drawerInactiveTintColor: "white",
+            drawerStyle: {
+              borderRightColor: "gray",
+              borderRightWidth: StyleSheet.hairlineWidth,
+            },
+          }}
+        >
+          <Drawer.Screen
+            name="index"
+            options={{
+              drawerLabel: "ChatGPT",
+              drawerIcon: () => (
+                <FontAwesome5 name="atom" size={18} color="white" />
+              ),
+            }}
+          />
+        </Drawer>
+      </ThemeProvider>
+    </GestureHandlerRootView>
+  );
 }
