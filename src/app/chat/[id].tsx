971ec87c410e7f546dc1a4b09f17a6e1655ab369
Farhan Shaikh <programmingwithfarhan@gmail.com>
2025-09-04 14:51:14 +0530

    Chat Screen

--- src/app/chat/[id].tsx
+++ src/app/chat/[id].tsx
@@ -1,11 +1,24 @@
 import { useLocalSearchParams } from "expo-router";
 import { Text, View } from "react-native";
+import chatHistory from "@assets/data/chatHistory.json";
 
 export default function ChatScreen() {
   const { id } = useLocalSearchParams();
+
+  const chat = chatHistory.find((item) => item.id === id);
+
+  if (!chat) {
+    return (
+      <View>
+        {" "}
+        <Text className="text-white">Chat not found</Text>
+      </View>
+    );
+  }
+
   return (
     <View>
-      <Text className="text-white">Chat Screen: {id}</Text>
+      <Text className="text-white">Chat screen: {chat.title}</Text>
     </View>
   );
 }
